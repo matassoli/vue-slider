d2a503a27864047300bca63ab1f20863d5765047
Nicolò Matassoli <nico.matassoli@gmail.com>
2021-03-25 17:42:24 +0100

    imgindex

--- js/myscript.js
+++ js/myscript.js
@@ -1,1 +1,20 @@
-console.log("hello");
+var app = new Vue({
+  el: "#app",
+  data: {
+    images: [
+      "https://images.pexels.com/photos/371633/pexels-photo-371633.jpeg?cs=srgb&dl=clouds-country-daylight-371633.jpg&fm=jpg",
+      "https://static.photocdn.pt/images/articles/2017/04/28/iStock-646511634.jpg",
+      "https://cdn.mos.cms.futurecdn.net/FUE7XiFApEqWZQ85wYcAfM.jpg",
+      "https://static.photocdn.pt/images/articles/2017/04/28/iStock-546424192.jpg",
+    ],
+    imageIndex: 0
+  },
+  methods: {
+    nextImg: function() {
+      this.imageIndex++;
+    },
+    prevImg: function() {
+      this.imageIndex--;
+    }
+  }
+});
